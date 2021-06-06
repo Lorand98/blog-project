065bb49d55ec3bfae8c8a22fbf3eb0f9b7a3663d
Lorand98 <kalmarlorand@yahoo.com>
2021-06-06 11:49:41 +0300

    EJS Challenge completed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const ejs = require("ejs");
+const _ = require("lodash");
+let posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,7 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -27,6 +32,46 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+// app.get("/posts/:postName", function (req, res) {
+//   const { postName } = req.params;
+//   const post = posts.find(
+//     (post) =>
+//       post.postTitle.split(" ").join("-").toLowerCase() ===
+//       postName.split(" ").join("-").toLowerCase()
+//   );
+//   if (post) res.render("post", { post: post });
+// });
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.postTitle);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        postTitle: post.postTitle,
+        postBody: post.postBody,
+      });
+    }
+  });
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    postTitle: req.body.postTitle,
+    slug: _.kebabCase(req.body.postTitle),
+    postBody: req.body.postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
